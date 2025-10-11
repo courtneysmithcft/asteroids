55c6b1031986e1602db9290366270c6e89b96a1a
courtneysmithcft <csmith8522@protonmail.com>
2025-10-11 14:52:35 -0500

    update constants and main.py

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        Surface.fill("black")
+        display.flip()
 
+	
 
 if __name__ == "__main__":
     main()
