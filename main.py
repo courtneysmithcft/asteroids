6d7ce7f28610dbd2608df21d222da4739994f3ed
courtneysmithcft <csmith8522@protonmail.com>
2025-10-11 15:02:34 -0500

    introduced background and game loop

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from constants import *
 
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
-        Surface.fill("black")
-        display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 	
 
